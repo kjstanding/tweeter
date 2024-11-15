628dc8151b8b96b2ff09d1faca49124ea2c74f62
Kyle Standing <kjstanding2@gmail.com>
2024-11-14 23:46:20 -0700

    Initial commit

---
+++ tweeter-web/src/components/authentication/AuthenticationFields.tsx
@@ -0,0 +1,38 @@
+
+interface Props {
+    setAlias: (alias: string) => void;
+    setPassword: (password: string) => void;
+    doOnEnter: (event: React.KeyboardEvent<HTMLElement>) => void;
+}
+
+const AuthenicationFields = (props: Props) => {
+    return (
+        <>
+        <div className="form-floating">
+          <input
+            type="text"
+            className="form-control"
+            size={50}
+            id="aliasInput"
+            placeholder="name@example.com"
+            onKeyDown={props.doOnEnter}
+            onChange={(event) => props.setAlias(event.target.value)}
+          />
+          <label htmlFor="aliasInput">Alias</label>
+        </div>
+        <div className="form-floating mb-3">
+          <input
+            type="password"
+            className="form-control bottom"
+            id="passwordInput"
+            placeholder="Password"
+            onKeyDown={props.doOnEnter}
+            onChange={(event) => props.setPassword(event.target.value)}
+          />
+          <label htmlFor="passwordInput">Password</label>
+        </div>
+      </>
+    );
+};
+
+export default AuthenicationFields;
